f046ea673b9e5bc20ac0c6a7876092b43f88fa54
Shin Hong <hongshin@gmail.com>
2023-11-20 10:13:34 +0900

    add reserved

--- test4/p2.c
+++ test4/p2.c
@@ -2,6 +2,16 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+char reserved[33][16] = {
+	"auto",	"else", "long",	"switch", "break", "enum", 
+	"register", "typedef", "case", "extern", "return",
+	"union", "char", "float", "short", "unsigned", "const",
+	"for", "signed", "void", "continue", "goto", "sizeof",
+	"volatile", "default", "if", "static", "while", "do",
+	"int", "struct", "_Packed", "double"
+} ;
+
+
 /*FIXME*/
 
 
